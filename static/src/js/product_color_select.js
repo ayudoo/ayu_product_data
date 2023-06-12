a538c08e23c61d72a4754fa0600fe7df88893fa8
Michael Jurke <m.jurke@gmx.de>
2023-06-12 18:07:28 +0200

    Workaround for non working tooltip delay

--- static/src/js/product_color_select.js
+++ static/src/js/product_color_select.js
@@ -1,9 +1,14 @@
+
+
+
 odoo.define('ayu_product_data.product_color_select', function(require) {
   "use strict";
   setTimeout(function() {
     require('web.dom_ready');
-    $('body').tooltip({
-      selector: '.product-color-select-tooltip'
-    });
+    $('.product-color-select-tooltip').tooltip({
+      delay: {
+        show: 0
+      }
+    })
   }, 0);
 });
